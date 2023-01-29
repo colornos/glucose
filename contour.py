d5ff36732a0a49c84c01afa70f0f81c0e4676beb
colornos <rivas432@hotmail.com>
2023-01-28 20:04:31 -0600

    Update contour.py

--- contour.py
+++ contour.py
@@ -1,6 +1,8 @@
 import logging
 import pygatt
-from pygatt.exceptions import NotConnectedError
+from pygatt.exceptions import NotConnectedError, NotificationTimeout
+import time
+from struct import unpack
 
 # Create a logger for logging debugging information
 log = logging.getLogger(__name__)
@@ -20,29 +22,40 @@ def connect_device(ble_address):
         log.error("Failed to connect to device")
         return None
 
+# Send RACP command to read all records
+def read_all_records(device):
+    try:
+        racp_handle = device.get_handle("00002a52-0000-1000-8000-00805f9b34fb")
+        device.char_write_handle(racp_handle, [1,1])
+    except pygatt.exceptions.NotConnectedError:
+        log.error("Failed to send RACP command to read all records")
+
+# Send RACP command to read last record received
+def read_last_record(device):
+    try:
+        racp_handle = device.get_handle("00002a52-0000-1000-8000-00805f9b34fb")
+        device.char_write_handle(racp_handle, [1,6])
+    except pygatt.exceptions.NotConnectedError:
+        log.error("Failed to send RACP command to read last record")
+
+# Send RACP command to read number of records
+def read_number_of_records(device):
+    try:
+        racp_handle = device.get_handle("00002a52-0000-1000-8000-00805f9b34fb")
+        device.char_write_handle(racp_handle, [4,1])
+    except pygatt.exceptions.NotConnectedError:
+        log.error("Failed to send RACP command to read number of records")
+
+# Send RACP command to read extract from record 45 onwards
+def read_extract_from_record(device):
+    try:
+        racp_handle = device.get_handle("00002a52-0000-1000-8000-00805f9b34fb")
+        device.char_write_handle(racp_handle, [1,3,1,45,0])
+    except pygatt.exceptions.NotConnectedError:
+        log.error("Failed to send RACP command to read extract from record 45")
+
 # Subscribe to the glucose measurement characteristic
 def subscribe_to_glucose(device):
     try:
         glucose_handle = device.get_handle("00002a18-0000-1000-8000-00805f9b34fb")
-        device.subscribe(glucose_handle, callback=process_glucose)
-    except pygatt.exceptions.NotConnectedError:
-        log.error("Failed to subscribe to glucose characteristic")
-
-# Process the glucose measurement received
-def process_glucose(handle, value):
-    # Parse the glucose measurement value
-    glucose = unpack("<h", value)[0] / 18.0
-    log.debug("Received glucose measurement: {}".format(glucose))
-
-# Main loop to read the latest glucose reading
-while True:
-    device = connect_device(ble_address)
-    if device:
-        subscribe_to_glucose(device)
-        log.debug("Waiting for glucose measurement...")
-        time.sleep(30)
-        try:
-            device.disconnect()
-        except NotConnectedError:
-            log.error("Failed to disconnect from device")
-            break
+        device.subscribe(glucose_handle
