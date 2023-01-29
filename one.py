a56824d1d738d79ab324c852837913ab0b52a276
colornos <rivas432@hotmail.com>
2023-01-28 20:21:22 -0600

    Update one.py

--- one.py
+++ one.py
@@ -1,10 +1,11 @@
 import logging
 import pygatt
-from pygatt.exceptions import NotConnectedError, NotificationTimeout
+from pygatt.exceptions import NotConnectedError, BLEError
 import time
 from struct import unpack
 
 # Create a logger for logging debugging information
+logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
 # Get the BLE address and device name from the configuration file
@@ -17,45 +18,55 @@ def connect_device(ble_address):
         adapter = pygatt.GATTToolBackend()
         adapter.start()
         device = adapter.connect(ble_address)
+        log.debug("Connected to device")
         return device
     except pygatt.exceptions.NotConnectedError:
         log.error("Failed to connect to device")
         return None
+    except BLEError as e:
+        log.error("Error connecting to device: {}".format(e))
+        return None
 
-# Send RACP command to read all records
-def read_all_records(device):
+# Send RACP command to the device
+def send_racp_command(device, command):
     try:
         racp_handle = device.get_handle("00002a52-0000-1000-8000-00805f9b34fb")
-        device.char_write_handle(racp_handle, [1,1])
+        device.char_write_handle(racp_handle, bytearray(command))
+        log.debug("Sent RACP command: {}".format(command))
     except pygatt.exceptions.NotConnectedError:
-        log.error("Failed to send RACP command to read all records")
-
-# Send RACP command to read last record received
-def read_last_record(device):
-    try:
-        racp_handle = device.get_handle("00002a52-0000-1000-8000-00805f9b34fb")
-        device.char_write_handle(racp_handle, [1,6])
-    except pygatt.exceptions.NotConnectedError:
-        log.error("Failed to send RACP command to read last record")
-
-# Send RACP command to read number of records
-def read_number_of_records(device):
-    try:
-        racp_handle = device.get_handle("00002a52-0000-1000-8000-00805f9b34fb")
-        device.char_write_handle(racp_handle, [4,1])
-    except pygatt.exceptions.NotConnectedError:
-        log.error("Failed to send RACP command to read number of records")
-
-# Send RACP command to read extract from record 45 onwards
-def read_extract_from_record(device):
-    try:
-        racp_handle = device.get_handle("00002a52-0000-1000-8000-00805f9b34fb")
-        device.char_write_handle(racp_handle, [1,3,1,45,0])
-    except pygatt.exceptions.NotConnectedError:
-        log.error("Failed to send RACP command to read extract from record 45")
+        log.error("Failed to send RACP command")
+    except BLEError as e:
+        log.error("Error sending RACP command: {}".format(e))
 
 # Subscribe to the glucose measurement characteristic
 def subscribe_to_glucose(device):
     try:
         glucose_handle = device.get_handle("00002a18-0000-1000-8000-00805f9b34fb")
-        device.subscribe(glucose_handle
+        device.subscribe(glucose_handle, callback=process_glucose)
+        log.debug("Subscribed to glucose characteristic")
+    except pygatt.exceptions.NotConnectedError:
+        log.error("Failed to subscribe to glucose characteristic")
+    except BLEError as e:
+        log.error("Error subscribing to glucose characteristic: {}".format(e))
+
+# Process the glucose measurement received
+def process_glucose(handle, value):
+    # Parse the glucose measurement value
+    glucose = unpack("<h", value)[0] / 18.0
+    log.debug("Received glucose measurement: {}".format(glucose))
+
+# Main loop to read the latest glucose reading
+while True:
+    device = connect_device(ble_address)
+    if device:
+        # Send RACP command to read the last glucose measurement
+        send_racp_command(device, [1, 6])
+        # Subscribe to glucose measurement notifications
+        subscribe_to_glucose(device)
+        log.debug("Waiting for glucose measurement...")
+        time.sleep(30)
+        try:
+            device.disconnect()
+            log.debug("Disconnected from device")
+        except NotConnectedError:
+            log.error("Failed to disconnect from device")
